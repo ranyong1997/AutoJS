107df93b42547b030effbfeb5a94a3f8e1bc01d7
冉勇の云 <1311518086@qq.com>
2022-02-15 20:39:41 +0800

    bug fixed

---
+++ Demo/script/debug.js
@@ -0,0 +1,20 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: 冉勇
+ * @Date: 2022-02-15 20:27:52
+ * @LastEditTime: 2022-02-15 20:27:53
+ */
+//显示控制台
+console.show();
+
+console.verbose("这是灰色");
+console.log("这是黑色");
+console.info("这是红色");
+console.warn("这是蓝色");
+console.error("这是绿色=_=");
+hey();
+
+function hey() {
+    console.trace("打印日志行数");
+}
